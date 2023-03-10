cbb4825956a85bde6465c48e61101493ad00398c
Itachi Uchiha <43094983+shiv30310@users.noreply.github.com>
2023-03-10 15:23:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 // complete the given function
-
+let string;
 function palindrome(str){
-
+	string = [str.split("")];
+	for (let index = 0; index < string.length; index++) {
+		if(string[index] == str[string.length - index]) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
 }
 module.exports = palindrome
