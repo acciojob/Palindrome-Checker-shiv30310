77442f1bdad8b512a67a3623958a646bb689ed74
Itachi Uchiha <43094983+shiv30310@users.noreply.github.com>
2023-03-10 15:38:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 // complete the given function
 let string;
 function palindrome(str){
-	string = [str.split("")];
+	string = str
 	for (let index = 0; index < string.length /2; index++) {
-		if(string[index] !== str[string.length - index -1]) {
+		if(string[index] !== string[string.length - index -1]) {
 			return false;
 		}
 		else {
