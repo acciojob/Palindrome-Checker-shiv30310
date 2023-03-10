9cf5989ff0a2d52495d7178e8086fec738233c56
Itachi Uchiha <43094983+shiv30310@users.noreply.github.com>
2023-03-10 15:35:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,12 +2,12 @@
 let string;
 function palindrome(str){
 	string = [str.split("")];
-	for (let index = 0; index < string.length; index++) {
-		if(string[index] == str[string.length - index]) {
-			return true;
+	for (let index = 0; index < string.length /2; index++) {
+		if(string[index] !== str[string.length - index -1]) {
+			return false;
 		}
 		else {
-			return false;
+			return true;
 		}
 	}
 }
